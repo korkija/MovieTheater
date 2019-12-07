d37667776ed2ff66f328a7baef4ae2a37f6b025e
Korkia <korkija@gmail.com>
2019-12-07 23:59:10 +0200

    отрисовка малого зала

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -3,12 +3,11 @@ import ReactDOM from "react-dom";
 import {getSessionSpace} from "../actions/ChooseMovie";
 import {connect} from "react-redux";
 
-class Modal extends React.Component{
-
+class Modal extends React.Component {
 
 
     componentWillMount() {
-        this.root=document.createElement("div");
+        this.root = document.createElement("div");
         const body = document.querySelector("body");
         body.appendChild(this.root);
     }
@@ -22,34 +21,45 @@ class Modal extends React.Component{
             movies
         } = this.props;
         movies.sessionSpace.sort(function (a, b) {
-            return (a.row-b.row);
+            return (a.row - b.row);
         });
         movies.sessionSpace.sort(function (a, b) {
-            return (a.row===b.row)&&(a.place - b.place);
+            return (a.row === b.row) && (a.place - b.place);
         });
         console.log("this.sessionSpacesessionSpace############################################");
-        console.log( movies.sessionSpace);
-        return  ReactDOM.createPortal(
+        console.log(movies.sessionSpace);
+
+        return ReactDOM.createPortal(
             <div className="portal-wrapper">
                 <div className="modal">
                     <h1>{this.props.title}</h1>
                     <button onClick={this.props.handleShow}>Close</button>
+                    <div className="row-place10x7 ">
+                        {movies.sessionSpace.map((item, i) => (
+                            <div className="seat" key={i}>{item.place}</div>
+                        ))
+
+                        }
+                    </div>
                 </div>
             </div>,
             this.root
-        );
+        )
     }
 }
 
-const mapStateToProps = (state) => ({
-    isLoading: state.isLoading,
-    movies: state.movies,
-    sessionSpace: state.moviesFilter,
-    chooseMovie: state.chooseMovie,
-});
-const mapDispatchToProps = {
-    getSessionSpace
-};
+
+const
+    mapStateToProps = (state) => ({
+        isLoading: state.isLoading,
+        movies: state.movies,
+        sessionSpace: state.moviesFilter,
+        chooseMovie: state.chooseMovie,
+    });
+const
+    mapDispatchToProps = {
+        getSessionSpace
+    };
 export const
     MySessionSpace = connect(
         mapStateToProps,
