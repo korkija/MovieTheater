9d13ba10f9afd59a43614e864166ff1529754105
Korkia <korkija@gmail.com>
2019-12-18 23:05:37 +0200

    fixing Modal.jsx

--- src/components/Button.jsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export const Button = ({ onClick, text }) => (
-    <button onClick={onClick}>{text}</button>
-);
--- src/components/Hello.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-
-export const Hello = () => {
-    const hello = "hello";
-    return (<h1>{hello}</h1>)
-};
